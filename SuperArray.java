d5603d863ae8ed78c6b617f8fd8763d89f771abd
i has candys <ichen10@stuy.edu>
2020-11-02 18:57:01 -0500

    Constructor, size, and add methods added to SuperArray class

---
+++ SuperArray.java
@@ -0,0 +1,18 @@
+import java.lang.String;
+public class SuperArray {
+	private String[] data;
+	private int size;
+
+	public SuperArray() {
+		data = new String[10];
+		size = 0;
+	}
+	public int size() {
+		return size;
+	}
+	public boolean add(String element) {
+		data[size] = element;
+		size++;
+		return true;
+	}
+}
---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+	public static void main(String[] args) {
+		SuperArray k = new SuperArray();
+		System.out.println(k.toString());
+		System.out.println(k.size());
+		k.add("lol");
+		System.out.println(k.toString());
+		System.out.println(k.size());
+    }
+}
